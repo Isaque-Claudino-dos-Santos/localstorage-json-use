e49098f1f095d38cf8f50f6fa752abc54c7f69d6
isaque claudino <isaqueclaudino12@gmail.com>
2023-02-25 22:24:50 -0300

    add typelocal

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { LocalStorage } from 'node-localstorage'
 import LocalStorageJson from './LocalStorageJson.js'
+import Local from './Local.js'
 
 global.localStorage = new LocalStorage('localStorage')
 
 const localStorageJson = new LocalStorageJson()
 
+export type TypeLocal = typeof Local
 export default localStorageJson
