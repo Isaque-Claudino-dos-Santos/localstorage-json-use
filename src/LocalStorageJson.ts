9c98ffde5946d0bed1841716678f52a9a53c6606
isaque claudino <isaqueclaudino12@gmail.com>
2023-02-25 23:20:44 -0300

    document new method

--- src/LocalStorageJson.ts
+++ src/LocalStorageJson.ts
@@ -1,10 +1,19 @@
+import { LocalStorage } from 'node-localstorage'
 import Local from './Local.js'
 
 export default class LocalStorageJSON {
+    constructor(public localStorageName: string) {
+        this.newLocalStorage(localStorageName)
+    }
     create<LocalObject>(
         localName: string,
         object: LocalObject
     ): Local<LocalObject> {
         return new Local<LocalObject>(localName, object as LocalObject)
+    }
+
+    newLocalStorage(name: string) {
+        global.localStorage = new LocalStorage(name)
+        this.localStorageName = name
     }
 }
