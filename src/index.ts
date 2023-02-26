6a83c38fde910c0d8d18fba419222e8d7e1dc4c9
isaque claudino <isaqueclaudino12@gmail.com>
2023-02-26 20:56:05 -0300

    ajust local name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import LocalStorageJson from './LocalStorageJson.js'
 import Local from './Local.js'
 
-const localStorageJson = new LocalStorageJson('LocalStorage')
+const localStorageJson = new LocalStorageJson('localStorage')
 
 export { Local }
 export default localStorageJson
